3f75da6d12c6ade95d47cc8ea5506251612991ba
Hillary <103367729+hzelas5685@users.noreply.github.com>
2023-02-24 14:33:11 -0500

    sad

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let package_i = document.getElementById('package_i');
 
-function changeColor() { package_i.style.accentColor = 'grey' }
+function changeColor() { package_i.style.width = "50%" }
 
 package_i.addEventListener('mouseover', changeColor)
