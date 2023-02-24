300f0be49d1ed4d788f734262d68ad17f7ffc22e
Hillary <103367729+hzelas5685@users.noreply.github.com>
2023-02-24 14:28:39 -0500

    asd

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert ('Hello World!')
+let package_i = document.getElementById('package_i');
+
+function changeColor() { package_i.style.accentColor = 'grey' }
+
+package_i.addEventListener('mouseover', changeColor)
