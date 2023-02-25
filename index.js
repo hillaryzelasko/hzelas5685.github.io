5d89c9f9c966fee13ea62af00eda4bf5fed6e66c
Hillary <103367729+hzelas5685@users.noreply.github.com>
2023-02-25 09:15:47 -0500

    push

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let package_i = document.getElementById('package_i');
 
-function changeColor() { package_i.style.width = '200px' }
+function changeColor() { package_i.style.width = '110%' }
 
 package_i.addEventListener('mouseover', changeColor)
 
