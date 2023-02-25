89f7e48214450a45cc75b43480c7db38ec1ff27b
Hillary <103367729+hzelas5685@users.noreply.github.com>
2023-02-25 09:39:56 -0500

    push

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 let package_i = document.getElementById('package_i');
+let package_ii = document.getElementById('package_ii')
+let package_iii = document.getElementById('package_iii')
 
-function changeColor() { package_i.style.width = '110%' }
-function reset() { package_i.style.width = ''}
+function changeSizePackage_I() { 
+    package_i.style.width = '110%';
+    package_ii.style.width = '90%';
+    package_iii.style.width = '90%';
 
-package_i.addEventListener('mouseover', changeColor)
+}
+function changeSizePackage_II() { 
+    package_i.style.width = '90%';
+    package_ii.style.width = '110%';
+    package_iii.style.width = '90%';
+
+}
+function changeSizePackage_III() { 
+    package_i.style.width = '90%';
+    package_ii.style.width = '90%';
+    package_iii.style.width = '110%';
+
+}
+
+function reset() { 
+    package_i.style.width = '';
+    package_ii.style.width = '';
+    package_iii.style.width = '';
+}
+
+package_i.addEventListener('mouseover', changeSizePackage_I)
 package_i.addEventListener('mouseout', reset)
 
+package_ii.addEventListener('mouseover', changeSizePackage_II)
+package_ii.addEventListener('mouseout', reset)
+
+package_iii.addEventListener('mouseover', changeSizePackage_III)
+package_iii.addEventListener('mouseout', reset)
